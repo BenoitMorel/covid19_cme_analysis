2fc5e75d22daf12efd3f3617ccd56ffa64db2757
Pierre Barbera <pierre.pbdas@gmail.com>
2020-04-02 16:42:21 +0200

    added epa outgroup rooting step

---
+++ pipeline/5_epa_outgroup_rooting.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import os
+import sys
+sys.path.insert(0, 'scripts')
+import common
+import epa_launcher
+import util
+
+version = util.get_version( sys.argv )
+
+tree = util.versioned_path( version, common.raxml_best_tree)
+modelfile = util.versioned_path( version, common.raxml_best_model)
+ref_msa = util.versioned_path( version, common.alignment)
+query_msa = util.versioned_path( version, common.outgroup_alignment)
+output = util.versioned_path( version, common.epa_rooting_dir )
+
+epa_launcher.launch_epa(tree, modelfile, ref_msa, query_msa, out_dir, thorough=True)
