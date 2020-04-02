bdcdaaa704e5de8ce3119901f4a34e739801287b
BenoitMorel <morelbt@hitssv543.villa-bosch.de>
2020-04-02 21:24:59 +0200

    Change the setup.sh to make it easier to enable/disable some steps

--- setup.sh
+++ setup.sh
@@ -1,10 +1,6 @@
+# Install all dependencies for the pipeline
 
-runs="runs"
-results="results"
-data="data"
 software="software"
-
-
 
 echo "Cloning software..."
 mkdir -p $software
@@ -16,50 +12,72 @@ git clone --recursive https://github.com/Pas-Kapli/mptp.git
 git clone --recursive https://github.com/Pbdas/epa-ng.git
 git clone https://github.com/lczech/genesis.git
 
-echo "Installing raxml-ng..."
-cd raxml-ng
-mkdir -p build && cd build
-cmake -DUSE_MPI=ON ..
-make -j
-cd ../..
+install_raxmlng() {
+  echo "Installing raxml-ng..."
+  cd raxml-ng
+  mkdir -p build && cd build
+  cmake -DUSE_MPI=ON ..
+  make -j
+  cd ../..
+}
 
-echo "Installing modeltest..."
-cd modeltest
-mkdir -p build && cd build
-cmake -DUSE_MPI=ON ..
-make -j
-cd ../..
+install_modeltest() {
+  echo "Installing modeltest..."
+  cd modeltest
+  mkdir -p build && cd build
+  cmake -DUSE_MPI=ON ..
+  make -j
+  cd ../..
+}
 
-echo "Installing pargenes..."
-cd ParGenes
-./install.sh 40
-cd ..
+install_pargenes() {
+  echo "Installing pargenes..."
+  cd ParGenes
+  ./install.sh 40
+  cd ..
+}
 
-echo "Installing mptp..."
-cd mptp 
-./autogen.sh
-./configure
-make
-cd ..
+install_mptp() {
+  echo "Installing mptp..."
+  cd mptp 
+  ./autogen.sh
+  ./configure
+  make
+  cd ..
+}
 
-echo "Installing epa-ng..."
-cd epa-ng
-make -j
-cd ..
+install_epa() {
+  echo "Installing epa-ng..."
+  cd epa-ng
+  make -j
+  cd ..
+}
 
-echo "Installing genesis..."
-cd genesis/apps
-ln -s ../../../scripts/*.cpp .
-cd ..
-make -j
-make update -j
-cd ..
+install_genesis() {
+  echo "Installing genesis..."
+  cd genesis/apps
+  ln -s ../../../scripts/*.cpp .
+  cd ..
+  make -j
+  make update -j
+  cd ..
+}
 
-echo "Installing IQtree..."
-wget https://github.com/Cibiv/IQ-TREE/releases/download/v1.6.12/iqtree-1.6.12-Linux.tar.gz
-tar -xzf iqtree-1.6.12-Linux.tar.gz
-mv iqtree-1.6.12-Linux iqtree
-rm iqtree-1.6.12-Linux.tar.gz
+install_iqtree() {
+  echo "Installing IQtree..."
+  wget https://github.com/Cibiv/IQ-TREE/releases/download/v1.6.12/iqtree-1.6.12-Linux.tar.gz
+  tar -xzf iqtree-1.6.12-Linux.tar.gz
+  mv iqtree-1.6.12-Linux iqtree
+  rm iqtree-1.6.12-Linux.tar.gz
+}
+
+install_raxmlng
+install_modeltest
+install_pargenes
+install_mptp
+install_epa
+install_genesis
+install_iqtree
 
 
 echo "Finished"
