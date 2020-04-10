95af7d875e67c6291473413cbc68492c5d9c865e
Pierre Barbera <pierre.pbdas@gmail.com>
2020-04-10 09:56:32 +0200

    updated the paths in pavlos preanalysis script

--- scripts/preanalysis1.sh
+++ scripts/preanalysis1.sh
@@ -1,3 +1,5 @@
+BASE=$(cd `dirname "${BASH_SOURCE[0]}"` && pwd)
+
 input=$1
 date=$2
 alignment=$3
@@ -5,11 +7,9 @@ alignment=$3
 echo $date
 echo $alignment
 
-binpath=~/cov/bin/
-
-$binpath/fasta2oneline.pl $input.fasta  > ${input}_oneline_${date}.fasta
-$binpath/getFullSeq.pl -in  ${input}_oneline_${date}.fasta -length 29000 > ${input}_oneline_fullseq_${date}.fasta
-$binpath/filterSequences.pl -in ${input}_oneline_fullseq_${date}.fasta > ${input}_oneline_fullseq_ns_${date}.fasta
+${BASE}/fasta2oneline.pl $input.fasta  > ${input}_oneline_${date}.fasta
+${BASE}/getFullSeq.pl -in  ${input}_oneline_${date}.fasta -length 29000 > ${input}_oneline_fullseq_${date}.fasta
+${BASE}/filterSequences.pl -in ${input}_oneline_fullseq_${date}.fasta > ${input}_oneline_fullseq_ns_${date}.fasta
 if [[ "$alignment" != "" ]]; then
     $alignment ${input}_oneline_fullseq_ns_${date}.fasta > ${input}_oneline_fullseq_ns_${date}.aln
 fi
