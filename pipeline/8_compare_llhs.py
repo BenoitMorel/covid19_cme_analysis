34c8f6e030955b4660d3190b3a00cdb5835f51b0
Lukas Hübner <huebnerlukas@gmail.com>
2020-04-14 12:39:50 +0200

    Fix include

--- pipeline/8_compare_llhs.py
+++ pipeline/8_compare_llhs.py
@@ -4,6 +4,8 @@ import sys
 sys.path.insert(0, 'scripts')
 import common
 import compare_llhs
+import jiggle_params
 paths = common.Paths( sys.argv )
 
 compare_llhs.evaluate_all_trees(paths)
+jiggle_params.evaluate_all(paths)
