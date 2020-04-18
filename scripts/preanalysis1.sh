ad6842478726963d0f2682e076c3c71856e6e4fa
Pierre Barbera <pierre.pbdas@gmail.com>
2020-04-18 14:10:01 +0200

    trimmed outgroup seqs as well

--- scripts/preanalysis1.sh
+++ scripts/preanalysis1.sh
@@ -21,7 +21,7 @@ ${scriptdir}/fasta2oneline.pl $input  > ${name}_oneline_${date}.fasta
 ${scriptdir}/getFullSeq.pl -in  ${name}_oneline_${date}.fasta -length 29000 > ${name}_oneline_fullseq_${date}.fasta
 ${scriptdir}/filterSequences.pl -in ${name}_oneline_fullseq_${date}.fasta > ${name}_oneline_fullseq_ns_${date}.fasta
 # ${scriptdir}/remove_outgroup.pl -in ${name}_oneline_fullseq_ns_${date}.fasta -outgroup "BAT|PANGOLIN" > ${name}_oneline_fullseq_ns_NOOUTGROUP_${date}.fasta
-${remove_outgroup} ${name}_oneline_fullseq_ns_${date}.fasta ${scriptdir}/../config/outgroups.txt ${name}_oneline_fullseq_ns_NOOUTGROUP_${date}.fasta covid_outgroups.fasta
+${remove_outgroup} ${name}_oneline_fullseq_ns_${date}.fasta ${scriptdir}/../config/outgroups.txt ${name}_oneline_fullseq_ns_NOOUTGROUP_${date}.fasta covid_outgroups_untrimmed.fasta
 
 if [[ "$alignment" != "" ]]; then
     # $alignment --thread $cores ${name}_oneline_fullseq_ns_${date}.fasta > ${name}_oneline_fullseq_ns_${date}.aln
@@ -33,4 +33,6 @@ if [[ "$alignment" != "" ]]; then
     $alignment --thread $cores ${name}_oneline_fullseq_ns_NOOUTGROUP_${date}.fasta > ${name}_oneline_fullseq_ns_NOOUTGROUP_${date}.aln
     ${scriptdir}/fasta2oneline.pl ${name}_oneline_fullseq_ns_NOOUTGROUP_${date}.aln > ${date}_nooutgroup.aln
     ${scriptdir}/trimalignment.pl -in ${date}_nooutgroup.aln -l 1000 > ${date}_nooutgroup_trimmed.aln
+    ${scriptdir}/fasta2oneline.pl covid_outgroups_untrimmed.fasta >  covid_outgroups_untrimmed_oneline.fasta
+    ${scriptdir}/trimalignment.pl -in covid_outgroups_untrimmed_oneline.fasta -l 1000 > covid_outgroups.fasta
 fi
