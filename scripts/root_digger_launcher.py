30245cf877552d0151ba73290042cd2d8e0eb7e4
computations <bwbettisworth@gmail.com>
2021-11-26 20:02:59 +0100

    Remove the MPI launcher from root digger

--- scripts/root_digger_launcher.py
+++ scripts/root_digger_launcher.py
@@ -5,17 +5,26 @@ import common
 import subprocess
 import util
 
-def launch_root_digger(tree, alignment, model, prefix, logfile, cores, factor = "1e20"):
-  cmd = ["mpiexec"]
-  cmd.extend(["-np", cores])
-  cmd.extend([common.root_digger])
-  cmd.extend(["--tree", tree])
-  cmd.extend(["--msa", alignment])
-  cmd.extend(["--model", model])
-  cmd.extend(["--exhaustive"])
-  cmd.extend(["--prefix", prefix])
-  cmd.extend(["--early-stop"])
-  #cmd.extend(["--verbose"])
-  print(cmd)
-  with open(logfile, 'w') as outfile:
-    subprocess.check_call(cmd, stdout = outfile, stderr = outfile)
+
+def launch_root_digger(tree,
+                       alignment,
+                       model,
+                       prefix,
+                       logfile,
+                       cores,
+                       factor="1e20"):
+    cmd = []
+    #cmd.extend["mpiexec"]
+    #cmd.extend(["-np", cores])
+    cmd.extend([common.root_digger])
+    cmd.extend(["--tree", tree])
+    cmd.extend(["--msa", alignment])
+    cmd.extend(["--model", model])
+    cmd.extend(["--exhaustive"])
+    cmd.extend(["--prefix", prefix])
+    cmd.extend(["--early-stop"])
+    cmd.extend(["--threads"])
+    #cmd.extend(["--verbose"])
+    print(cmd)
+    with open(logfile, 'w') as outfile:
+        subprocess.check_call(cmd, stdout=outfile, stderr=outfile)
